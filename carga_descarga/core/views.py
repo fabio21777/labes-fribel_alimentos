f888ddd0472829084e80962b9a4354b728303b3f
Marcio Magalhaes <marciorrmagalhaes@gmail.com>
2020-11-19 08:33:49 -0300

    primeiro Commit(Teste)
    primeiro Commit(Teste)

---
+++ carga_descarga/core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def login(request):
+    return render(request,'login.html')
+def acomp(request):
+    return render(request,'acomp.html')
+# Create your views here.
