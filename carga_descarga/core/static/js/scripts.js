34e2431a9ec7629e58cebfb1e5a5de34b363be45
YannFigueiredo <yann.fabricio@hotmail.com>
2020-11-27 11:17:31 -0300

    Adicionado jquery no projeto

--- carga_descarga/core/static/js/scripts.js
+++ carga_descarga/core/static/js/scripts.js
@@ -1,1 +1,10 @@
-console.log("funcionou");
+$(document).ready(function(){
+    var baseUrl = 'http://localhost:8000/';
+    var filter = $('#filter');
+
+    (filter).change(function() {
+        var filter = $(this).val();
+        console.log(filter);
+        //window.location.href = baseUrl + '?filter=' + filter;
+    });
+});
--- carga_descarga/core/views.py
+++ carga_descarga/core/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 from .models import Carga
 
 def acomp(request, usuario):
-    cargas = Carga.objects.all().order_by('-created_at')
+    filter = request.GET.get('filter')
+
+    if filter:
+        cargas = Carga.objects.filter(done=filter, user=request.user) 
+    else: 
+        cargas = Carga.objects.all().order_by('-created_at').filter(user=request.user)
 
     return render(request,'core/acomp.html', {'usuario': usuario, 'cargas': cargas})
 
