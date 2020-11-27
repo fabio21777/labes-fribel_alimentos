24a0d17105145c2f4e49e24bf455b4af244d4cb2
fabricio <fabriciosouza21777@gmail.com>
2020-11-27 14:56:00 -0300

    add o modelo box

--- carga_descarga/core/admin.py
+++ carga_descarga/core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Carga
+from .models import Box
 
 admin.site.register(Carga)
+admin.site.register(Box)
--- carga_descarga/core/models.py
+++ carga_descarga/core/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Box (models.Model):
+    box_name = models.CharField('name',max_length=20)
+          
+    def __str__(self):        
+        return self.box_name
 
 class Carga(models.Model):
     STATUS = (
@@ -12,6 +19,7 @@ class Carga(models.Model):
     dia_descarga  = models.DateField('Dia da descarga')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField('Status', max_length=15, choices=STATUS)
+    box = models.CharField('Box',default=' ',max_length=20,null=False)
 
     #Sempre que um registro for criado essa variável determina a data no BD
     created_at = models.DateTimeField(auto_now_add=True)
