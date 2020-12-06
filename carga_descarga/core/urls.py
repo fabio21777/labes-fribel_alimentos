080d97e45a7ea7d862b369bfb71bc4ac519a06c7
fabio21777 <fabiosouza21777@gmail.com>
2020-12-05 22:35:40 -0300

    update base.html, urls.py

--- carga_descarga/core/urls.py
+++ carga_descarga/core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('acompanhamento/<str:usuario>', views.acomp, name = 'acompanhamento'),
     path('acompanhamento/adicionarCarga/', views.addCarga, name = 'adicionar-carga'),
     path('acompanhamento/adicionarCarga/submit',views.set_carga, name = 'set-carga'),
+    path('acompanhamento/liberarCarga/<str:id>/',views.liberarCarga),
     path('liberar',views.liberar_carga, name = 'liberar-carga'),
     path('liberar/<str:id>/',views.liberar, name = 'liberar'),
     path('', views.login, name = 'login'), #Tela home
